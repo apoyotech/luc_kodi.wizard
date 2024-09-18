80a117af67baf87989397b0456ad10da2fff0d01
apoyotech <luciangazdaru@gmail.com>
2024-09-19 00:11:11 +0200

    Delete uservar.py

--- uservar.py
+++
@@ -1,8 +0,0 @@
-import xbmcaddon
-addon_id = xbmcaddon.Addon().getAddonInfo('id')
-
-'''#####-----Build File-----#####'''
-buildfile = 'http://CHANGEME'
-
-'''#####-----Excludes-----#####'''
-excludes  = ['plugin.video.whatever']
