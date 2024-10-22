29299aa7b715c0b59bb1f7df94c3c2185cf1556d
apoyotech <luciangazdaru@gmail.com>
2024-10-22 22:25:47 +0200

    Delete uservar.py

--- uservar.py
+++
@@ -1,8 +0,0 @@
-'''#####-----Build File-----#####'''
-buildfile = 'https://raw.githubusercontent.com/apoyotech/luc_kodi.wizard/main/resources/texts/builds.xml'
-
-'''#####-----Notifications File-----#####'''
-notify_url  = 'http://CHANGEME'
-
-'''#####-----Excludes-----#####'''
-excludes  = 'plugin.program.luc_kodi.wizard'
