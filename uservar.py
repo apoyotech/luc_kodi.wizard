e8b06308219111b8ebe709d4ee92afba3214e3be
apoyotech <luciangazdaru@gmail.com>
2024-10-22 22:21:29 +0200

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -1,8 +1,8 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://raw.githubusercontent.com/apoyotech/luc_kodi.wizard/refs/heads/main/resources/texts/builds.xml'
+buildfile = 'https://raw.githubusercontent.com/apoyotech/luc_kodi.wizard/main/resources/texts/builds.xml'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://CHANGEME'
 
 '''#####-----Excludes-----#####'''
-excludes  = ['plugin.program.luc_kodi.wizard']
+excludes  = 'plugin.program.luc_kodi.wizard'
