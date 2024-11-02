e482b3a934adc5db9d5e2b38e538ad05c0b4b98f
apoyotech <luciangazdaru@gmail.com>
2024-11-03 00:41:27 +0100

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -7,4 +7,4 @@ ART = os.path.join(PATH, 'resources', 'media')
 autointall='Yes'
 buildfile ='https://raw.githubusercontent.com/apoyotech/luc_kodi.wizard/refs/heads/main/resources/texts/builds.xml'
 notification= 'http://'
-excludes  = [addon_id, 'plugin.program.luc_kodi.wizard']
+excludes  = ['plugin.program.luc_kodi.wizard']
