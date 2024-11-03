db8110df5328b62de71559fce25abc36f3558d61
apoyotech <luciangazdaru@gmail.com>
2024-11-03 03:28:07 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -3,9 +3,9 @@ import sys
 from resources.lib.modules.plugin import router
  
 if __name__=='__main__':
-_handle = int(sys.argv[1])
+_handle = router(sys.argv[1])
 _params = sys.argv[2][1:]
 
-dispatcher = router.Router()
+
 
 
