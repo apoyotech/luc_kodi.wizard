320f15f38ac4c9eacd3dd7a8eef826910a2d0bc5
apoyotech <luciangazdaru@gmail.com>
2024-11-03 02:01:07 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -2,13 +2,7 @@
 if __name__ == '__main__':
   import sys
   from resources.libs.common import router
-
-    _handle = int(sys.argv[1])
-    _params = sys.argv[2][1:]
-    
-    
-   
-  
+_params = sys.argv[2][1:]
    from resources.lib.modules.plugin import build_install
 
 
