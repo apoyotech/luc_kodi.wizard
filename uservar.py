160b9aec35e479082a6113d0021dbc784a427df5
apoyotech <luciangazdaru@gmail.com>
2024-12-16 03:20:15 +0100

    Add files via upload

---
+++ uservar.py
@@ -0,0 +1,8 @@
+'#####-----Build File-----#####'''
+buildfile = 'https://mia.nl.tab.digital/s/FM2EjARN2FZMTAo/download/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'http://CHANGEME'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
