cd1fef5a3396542f26081e4f978b6542d15f5bdc
apoyotech <luciangazdaru@gmail.com>
2024-12-16 01:21:08 +0100

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -1,5 +1,5 @@
 '''#####-----Build File-----#####'''
-buildfile =''
+buildfile ='https://mia.nl.tab.digital/s/yNQbGLYZRy6pTNN/luc_kodi.backups/builds.xml'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://CHANGEME'
