ebdc442fcee2233d90d39f51b7b575c3da77eee8
apoyotech <luciangazdaru@gmail.com>
2025-03-10 11:13:22 +0100

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -1,5 +1,5 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://www.dropbox.com/scl/fi/gfsl8sgxofc8yi6gmf1z7/builds.xml?rlkey=pw5mh7vmlbvmehf6upn86rk8g&st=1tf3pmip&dl=0'
+buildfile = ''
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://CHANGEME'
