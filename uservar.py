3ae980bef90376624b31677da3b2f52c1d8f47df
apoyotech <luciangazdaru@gmail.com>
2025-03-10 12:29:43 +0100

    Delete uservar.py

--- uservar.py
+++
@@ -1,8 +0,0 @@
-'''#####-----Build File-----#####'''
-buildfile = 'https://mia.nl.tab.digital/s/NgbNnMoFedEKTkr/download/builds.xml'
-
-'''#####-----Notifications File-----#####'''
-notify_url  = 'http://CHANGEME'
-
-'''#####-----Excludes-----#####'''
-excludes  = ['plugin.video.whatever']
