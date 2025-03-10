8e79952ea20af7c01bb67d483b180c82b07952ec
apoyotech <luciangazdaru@gmail.com>
2025-03-10 11:24:27 +0100

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -1,5 +1,5 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://archive.org/details/builds_202503'
+buildfile = '<iframe src="https://archive.org/embed/builds_202503" width="560" height="384" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://CHANGEME'
