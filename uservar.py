b2d523b582e1e12692d3ff5728c7f9a6888dd38d
apoyotech <luciangazdaru@gmail.com>
2025-03-25 00:38:27 +0100

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -1,5 +1,5 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://mia.nl.tab.digital/s/aTc9CQcFa4bftX3/download/builds.xml'
+buildfile = 'https://mia.nl.tab.digital/s/fM7Y2Wg7d9dSGr9/download/builds.xml'
 
 '''#####-----Videos File-----#####'''
 videos_url = 'http://CHANGEME'
