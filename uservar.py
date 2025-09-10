f21e62416b27f0ad2c2c6e09113db2a40061da74
apoyotech <luciangazdaru@gmail.com>
2025-09-10 12:11:43 +0200

    Update uservar.py

--- uservar.py
+++ uservar.py
@@ -1,5 +1,5 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://mia.nl.tab.digital/s/DHBnjWdz6KwjscR/download/builds.xml'
+buildfile = 'https://mia.nl.tab.digital/s/cyX3PwEypzWqJkN/download/builds.xml'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://CHANGEME'
